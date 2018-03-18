5d58b69843b7b22774d186d47f8e0cdbf198bb6b
maverick-madhes <madheswaran1999@gmail.com>
2018-03-18 17:50:30 +0530

    solved cannot get /

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 
 
 
