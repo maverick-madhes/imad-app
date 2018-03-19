8547f7e14ba8ad5e9e1a6ca385859c90d389cae7
maverick-madhes <madheswaran1999@gmail.com>
2018-03-19 16:16:50 +0530

    deleted pages from ui folder

--- server.js
+++ server.js
@@ -5,25 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    one : {
+      title: 'one',
+      heading: 'page one',
+      content: `<div class="container">
+     hello world...i unconciously typed hello world.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository. 
+    </div>
+    <div>
+    <p>
+        hello world.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
+       </p>
+    <table border="1">
+    <tr>
+    <td>one</td>
+    <td>two</td>
+    </tr>
+    </table>
+    
+    </div>`
+    },
+    two : {
+    title: 'two',
+     heading: 'page two',
+    content: `<div class="container">
 
-var one = {
-  title : 'one',
-  heading: 'page one',
-  content : `<div class="container">
- hello world...i unconciously typed hello world.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository. 
-</div>
-<div>
-<p>
-    hello world.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.Files on the sidebar represent the source code of your web app. These files are all actually saved in a git repository on your github account. This console allows you to edit these files, deploy your app, and save these files back to your github repository.
-   </p>
-<table border="1">
-<tr>
-<td>one</td>
-<td>two</td>
-</tr>
-</table>
-
+	<p>
+		here we go...lets try it out 
+	</p>
+	<form>
+	<input type="submit"/>submit
+	</form>
+    </div>
+    </body>`
+    },
+    three : {
+          title: 'three',
+      heading: 'page three',
+      content: `<div class="container">
+    <p>
+        bla bla bla bla bla ...wow i ty[e d so fast...but there is a typo...fuck
+    </p>
 </div>`
+    },
 };
 
 function createTemplate (data){
@@ -49,17 +73,11 @@ function createTemplate (data){
     
     return htmlTemplate;
 }
-app.get('/one', function (req, res) {
-  res.send(createTemplate(one));
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-two.html'));
-});
-
-app.get('/three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -74,8 +92,6 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
@@ -83,3 +99,4 @@ var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
