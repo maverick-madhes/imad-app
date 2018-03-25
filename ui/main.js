a249cde7405ba52d59ed26e3a62706980275d880
maverick-madhes <madheswaran1999@gmail.com>
2018-03-26 00:51:12 +0530

    request to the counter end point

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,19 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
 
+button.onclick= function(){
+//create a request
+var request= new XMLHttpRequest();
+
+//capture the response 
+request.onreadystatechange = function (){
+    if (request.readyState === XMLHttpRequest.DONE)
+        if (request.status=== 200){
+            var counter =request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+}
+//make the request
+request.open('GET','http://localhost:8080/counter',true);
+request.send(null);
+};
