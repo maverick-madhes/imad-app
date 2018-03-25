1f07f7c77a4ad95cc58afec138ab8b67f2d9c660
maverick-madhes <madheswaran1999@gmail.com>
2018-03-26 03:12:43 +0530

    capture the response and render it in the main  page but still not working

--- ui/main.js
+++ ui/main.js
@@ -19,15 +19,20 @@ request.send(null);
 };
 
 //submit name 
-var nameInput = document.getElementById('name');
-var name= nameInput.value;
+
 var submit= document.getElementById('btn');
 submit.onclick=function(){
-    var names = ['name1','name2','name3','name4'];
+    var names = request.responseText;
+    names=JSON.parse(names);
     var list='';
     for (var i=0; i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML=list;
+    var nameInput = document.getElementById('name');
+    var name= nameInput.value;
+
 }
+request.open('GET','http://localhost:8080/submit?name=' + name,true);
+request.send(null);
