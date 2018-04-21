406091a029fb9138c6164dcfbffdb30f4f6d546d
maverick-madhes <madheswaran1999@gmail.com>
2018-04-22 04:59:11 +0530

    added a semi

--- ui/main.js
+++ ui/main.js
@@ -6,13 +6,14 @@ var request= new XMLHttpRequest();
 
 //capture the response 
 request.onreadystatechange = function (){
-    if (request.readyState === XMLHttpRequest.DONE)
+    if (request.readyState === XMLHttpRequest.DONE){
         if (request.status === 200){
             var counter =request.responseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
-}
+    }
+};
 //make the request
 request.open('GET','http://http://madheswaran1999.imad.hasura-app.io/counter',true);
 request.send(null);
