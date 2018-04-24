20ad0a56437b239bf8683e0ce7312fa8576b65ba
maverick-madhes <madheswaran1999@gmail.com>
2018-04-24 14:21:54 +0530

    created create user end point

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
   user : 'madheswaran1999',
@@ -13,6 +14,7 @@ var config = {
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var names=[];
 app.get('/submit', function(req, res) {
@@ -64,6 +66,8 @@ function createTemplate (data){
     
     return htmlTemplate;
 }
+
+
 app.get('/article/:articleName', function (req, res) {
   Pool.query('select * from article where title = $1' + [req.params.articleName] , function(err,result){
     if (err){
@@ -89,6 +93,24 @@ app.get('/hash/:input', function(req,res){
   res.send(hashedString);
 
 });
+
+app.post('/create-user', function (req, res) {
+  // username, password
+  // {"username": "tanmai", "password": "password"}
+  // JSON
+  var username = req.body.username;
+  var password = req.body.password;
+  var salt = crypto.randomBytes(128).toString('hex');
+  var dbString = hash(password, salt);
+  pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         res.send('User successfully created: ' + username);
+     }
+  });
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
